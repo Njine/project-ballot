228155a1876e6b61338b7920d478300c280dcb7a
njine <brikab@gmail.com>
2024-08-18 21:19:12 +0300

    added scripts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,8 +8,15 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   }
 });
 
+const privateKey = process.env.PRIVATE_KEY || ""; // Set a default value if PRIVATE_KEY is undefined
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+      sepolia: {
+          url: "https://ethereum-sepolia-rpc.publicnode.com",
+          accounts: [privateKey], // Use the defined privateKey variable
+      },
+  },
 };
 
 export default config;
