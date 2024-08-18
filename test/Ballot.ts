cb6f1cf0f1eb5323f97579f6a807b721a3c3ad09
njine <brikab@gmail.com>
2024-08-18 23:26:24 +0300

    Respawn

--- test/Ballot.ts
+++ test/Ballot.ts
@@ -1,3 +1,4 @@
+/*
 import { expect } from "chai";
 import { toHex, hexToString } from "viem";
 import { viem } from "hardhat";
@@ -118,3 +119,4 @@ describe("Ballot", () => {
         });
     });
 });
+*/
